ce441b2f5596c1ae32054af92b3d304a9605aa7d
Tianyu Cheng <tianyu.cheng@utexas.edu>
2024-10-01 17:02:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Prompt-Font-Header
+==================
+
+This repository is created for [PromptFont](https://shinmera.github.io/promptfont/),
+which is designed for button prompts (especially for games).
+
+It already comes with headers for multiple language bindings.
+This repository is presenting the headers in a different style,
+similar to [IconFontCppHeaders](https://github.com/juliettef/IconFontCppHeaders).
+
+Author(s)
+---------
+[Tianyu Cheng](tianyu.cheng@utexas.edu)
---
+++ generate.py
@@ -0,0 +1,40 @@
+import os
+import json
+
+def process(glyph: dict):
+    name = glyph.get("code-name", "")
+    name = name.replace("-", "_")
+    name = name.upper()
+    code = glyph.get("codepoint", "")
+    code = chr(code).encode("utf-8")
+    formatted = "\\x".join([f"{b:02x}" for b in code])
+    formatted = f"\\x{formatted}"
+    return {
+        "name": f"PF_{name}",
+        "code": glyph.get("code", ""),
+        "formatted": formatted,
+    }
+
+metadata = os.path.join("promptfont", "glyphs.json")
+with open(metadata, "r", encoding="utf-8") as f:
+    glyphs = json.load(f)
+    glyphs = [process(glyph) for glyph in glyphs]
+
+source = os.path.join("include", "promptfont.h")
+with open(source, "w") as f:
+    print("#ifndef PROMPT_FONT_H", file=f)
+    print("#define PROMPT_FONT_H", file=f)
+    print(file=f)
+    print('#define FONT_ICON_FILE_NAME_PROMPT "promptfont.ttf"', file=f)
+    print(file=f)
+    print("// clang-format off", file=f)
+    align = max([len(glyph["name"]) for glyph in glyphs])
+    for glyph in glyphs:
+        name = glyph["name"]
+        code = glyph["code"]
+        format = glyph["formatted"]
+        padding = " " * (align - len(name) + 2)
+        print(f'#define {name}{padding} \"{format}\" // {code}', file=f)
+    print("// clang-format on", file=f)
+    print(file=f)
+    print("#endif // PROMPT_FONT_H", file=f)
